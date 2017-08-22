f5332c716d63fbffc91cac3a15f4bf053e596d44
Joannis Orlandos <joannis@orlandos.nl>
2017-08-22 15:43:44 +0200

    move to beta branch

--- Vapor3Press/Package.swift
+++ Vapor3Press/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "VaporPress",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .revision("crash-fix")),
+        .package(url: "https://github.com/vapor/vapor.git", .revision("beta")),
         .package(url: "https://github.com/openkitten/meow.git", .revision("0.0.20170720")),
     ],
     targets: [
